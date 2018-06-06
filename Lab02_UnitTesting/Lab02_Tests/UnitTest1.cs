66c355835a5791d2010a51825fe72e69aba3a353
Jesse Atay <jesse.atay@gmail.com>
2018-06-06 11:46:13 -0700

    Initial file structure

---
+++ Lab02_UnitTesting/Lab02_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Lab02_UnitTesting;
+using Xunit;
+
+namespace Lab02_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Lab02_UnitTesting/Lab02_UnitTesting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab02_UnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Bank");
+            ChooseOptions();
+        }
+
+        public static double TotalBalance = 5000;
+
+        public static void ChooseOptions()
+        {
+            Console.WriteLine("Please choose an option.");
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Withdraw");
+            Console.WriteLine("4. Exit");
+        }
+
+        public static double CheckBalance()
+        {
+            Console.WriteLine($"Your current balance is {TotalBalance}");
+            return TotalBalance;
+        }
+    }
+}
