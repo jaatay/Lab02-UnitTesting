6d3a10273faca139a537c34a8ede9d8f28eebab4
Jesse Atay <jesse.atay@gmail.com>
2018-06-06 11:54:41 -0700

    updates to initial functions

--- Lab02_UnitTesting/Lab02_UnitTesting/Program.cs
+++ Lab02_UnitTesting/Lab02_UnitTesting/Program.cs
@@ -12,13 +12,22 @@ namespace Lab02_UnitTesting
 
         public static double TotalBalance = 5000;
 
+        public static void RunUntilQuit()
+        {
+            while 
+        }
+
         public static void ChooseOptions()
         {
-            Console.WriteLine("Please choose an option.");
+            Console.WriteLine("What would you like to do?");
             Console.WriteLine("1. Check Balance");
             Console.WriteLine("2. Deposit");
-            Console.WriteLine("3. Withdraw");
+            Console.WriteLine("3. Withdrawal");
             Console.WriteLine("4. Exit");
+
+            int UserChoice = Int32.Parse(Console.ReadLine());
+
+           
         }
 
         public static double CheckBalance()
